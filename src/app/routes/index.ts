6b6d6779e09a918de2c8435fc23548a864d8e2d6
parvez13913 <parvezz13913@gmail.com>
2023-09-14 06:06:36 +0600

    [DONE] setup

---
+++ src/app/routes/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+const router = express.Router();
+
+const moduleRoutes = [
+  {
+    path: "",
+    routes: "",
+  },
+];
+
+moduleRoutes.forEach((route) => router.use(route.path, route.routes));
+export default router;
