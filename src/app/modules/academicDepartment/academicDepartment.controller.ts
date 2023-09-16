b73961862bd31a7a504e44fa051eb16c22bc8c80
parvez13913 <parvezz13913@gmail.com>
2023-09-16 12:33:47 +0600

    [DONE] get all departments

--- src/app/modules/academicDepartment/academicDepartment.controller.ts
+++ src/app/modules/academicDepartment/academicDepartment.controller.ts
@@ -1,8 +1,11 @@
 import { AcademicDepartment } from '@prisma/client';
 import { Request, Response } from 'express';
 import httpStatus from 'http-status';
+import { paginationFields } from '../../../constants/pagination';
 import catchAsync from '../../../shared/catchAsync';
+import pick from '../../../shared/pick';
 import sendResponse from '../../../shared/sendResponse';
+import { academicDepartmentFilterableFields } from './academicDepartment.constants';
 import { AcademicDepartmentService } from './academicDepartment.service';
 
 const createAcademicDepartment = catchAsync(
@@ -20,6 +23,24 @@ const createAcademicDepartment = catchAsync(
   },
 );
 
+const getAllDepartMents = catchAsync(async (req: Request, res: Response) => {
+  const filters = pick(req.query, academicDepartmentFilterableFields);
+  const options = pick(req.query, paginationFields);
+  const result = await AcademicDepartmentService.getAllDepartMents(
+    filters,
+    options,
+  );
+
+  sendResponse<AcademicDepartment[]>(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Academic Department Created Successfully',
+    meta: result.meta,
+    data: result.data,
+  });
+});
+
 export const AcademicDepartmentController = {
   createAcademicDepartment,
+  getAllDepartMents,
 };
--- src/app/modules/academicDepartment/academicDepartment.service.ts
+++ src/app/modules/academicDepartment/academicDepartment.service.ts
@@ -1,5 +1,11 @@
-import { AcademicDepartment } from '@prisma/client';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { AcademicDepartment, Prisma } from '@prisma/client';
+import { paginationHelpers } from '../../../helpers/paginationHelper';
+import { IGenericResponse } from '../../../interfaces/common';
+import { IPaginationOptions } from '../../../interfaces/pagination';
 import prisma from '../../../shared/prisma';
+import { academicDepartmentFilterableFields } from './academicDepartment.constants';
+import { IAcademicDepartmentFilters } from './academicDepartment.intrface';
 
 const createAcademicDepartment = async (
   data: AcademicDepartment,
@@ -8,6 +14,62 @@ const createAcademicDepartment = async (
   return result;
 };
 
+const getAllDepartMents = async (
+  filters: IAcademicDepartmentFilters,
+  options: IPaginationOptions,
+): Promise<IGenericResponse<AcademicDepartment[]>> => {
+  const { searchTerm, ...filtersData } = filters;
+  const { page, limit, skip } = paginationHelpers.calculatePagination(options);
+
+  const andConditions = [];
+
+  if (searchTerm) {
+    andConditions.push({
+      OR: academicDepartmentFilterableFields.map(field => ({
+        [field]: {
+          contains: searchTerm,
+          mode: 'insensitive',
+        },
+      })),
+    });
+  }
+
+  if (Object.keys(filtersData).length > 0) {
+    andConditions.push({
+      AND: Object.keys(filtersData).map(key => ({
+        [key]: {
+          equals: (filtersData as any)[key],
+        },
+      })),
+    });
+  }
+
+  const whereConditions: Prisma.AcademicDepartmentWhereInput =
+    andConditions.length > 0 ? { AND: andConditions } : {};
+
+  const result = await prisma.academicDepartment.findMany({
+    where: whereConditions,
+    skip,
+    take: limit,
+    orderBy:
+      options.sortBy && options.sortOrder
+        ? { [options.sortBy]: options.sortOrder }
+        : { createdAt: 'desc' },
+  });
+
+  const total = await prisma.academicDepartment.count();
+
+  return {
+    meta: {
+      page,
+      limit,
+      total,
+    },
+    data: result,
+  };
+};
+
 export const AcademicDepartmentService = {
   createAcademicDepartment,
+  getAllDepartMents,
 };
