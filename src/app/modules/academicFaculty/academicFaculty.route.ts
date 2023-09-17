c64d6051ea7293eb8432cd8e44152d1c36aa6480
parvez13913 <parvezz13913@gmail.com>
2023-09-17 13:37:09 +0600

    [DONE] Create Academic Faculty validation

--- src/app/modules/academicFaculty/academicFaculty.route.ts
+++ src/app/modules/academicFaculty/academicFaculty.route.ts
@@ -1,10 +1,13 @@
 import express from 'express';
+import validateRequest from '../../middlewares/validateRequest';
 import { AcademicFacultyController } from './academicFaculty.controller';
+import { AcademicFacultyValidation } from './academicFaculty.validation';
 
 const router = express.Router();
 
 router.post(
   '/create-academicFaculty',
+  validateRequest(AcademicFacultyValidation.createAcademicFacultyZodSchema),
   AcademicFacultyController.createAcademicFaculty,
 );
 
