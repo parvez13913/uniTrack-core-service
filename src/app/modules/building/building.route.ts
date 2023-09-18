51ef4d9392c04d243de0f1b479b3e10585555e81
parvez13913 <parvezz13913@gmail.com>
2023-09-18 15:27:52 +0600

    [DONE] building validation

--- src/app/modules/building/building.route.ts
+++ src/app/modules/building/building.route.ts
@@ -1,9 +1,18 @@
 import express from 'express';
+import { ENUM_USER_ROLE } from '../../../enums/user';
+import auth from '../../middlewares/auth';
+import validateRequest from '../../middlewares/validateRequest';
 import { BuildingController } from './building.controller';
+import { BuildingValidation } from './building.validation';
 
 const router = express.Router();
 
-router.post('/create-building', BuildingController.createBuilding);
+router.post(
+  '/create-building',
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  validateRequest(BuildingValidation.createBuildingZodSchema),
+  BuildingController.createBuilding,
+);
 router.get('/', BuildingController.getAllBuilding);
 
 export const BuildingRouter = router;
