e4c51cd09b67c35c1223c6ba20953afbb0b541a5
parvez13913 <parvezz13913@gmail.com>
2023-09-19 15:09:08 +0600

    [DONE] course created

--- src/app/modules/course/course.service.ts
+++ src/app/modules/course/course.service.ts
@@ -1,12 +1,60 @@
-import { Course } from '@prisma/client';
+/* eslint-disable no-console */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import httpStatus from 'http-status';
+import ApiError from '../../../errors/ApiError';
 import prisma from '../../../shared/prisma';
+import { ICourseCreateData } from './course.interface';
 
-const createCourse = async (data: Course): Promise<Course> => {
-  const result = await prisma.course.create({
-    data,
+const createCourse = async (data: ICourseCreateData): Promise<any> => {
+  const { prerequisiteCourses, ...courseData } = data;
+  const newCourses = await prisma.$transaction(async transactionClient => {
+    const result = await transactionClient.course.create({
+      data: courseData,
+    });
+
+    if (!result) {
+      throw new ApiError(httpStatus.BAD_REQUEST, 'Unable to create Course');
+    }
+
+    if (prerequisiteCourses && prerequisiteCourses.length > 0) {
+      for (let index = 0; index < prerequisiteCourses.length; index++) {
+        const createPrerequisite =
+          await transactionClient.courseToPrereqisite.create({
+            data: {
+              courseId: result.id,
+              prerequisiteId: prerequisiteCourses[index].courseId,
+            },
+          });
+
+        console.log(createPrerequisite);
+      }
+    }
+    return result;
   });
 
-  return result;
+  if (newCourses) {
+    const responseData = await prisma.course.findUnique({
+      where: {
+        id: newCourses.id,
+      },
+      include: {
+        prerequisite: {
+          include: {
+            prerequistite: true,
+          },
+        },
+        prerequisiteFor: {
+          include: {
+            course: true,
+          },
+        },
+      },
+    });
+
+    return responseData;
+  }
+
+  throw new ApiError(httpStatus.BAD_REQUEST, 'Unable to create Course');
 };
 
 export const CourseService = {
