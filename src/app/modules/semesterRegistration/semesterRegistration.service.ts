e0dc77624d02efdaf067d2a0c02e2e4f9e17fb92
parvez13913 <parvezz13913@gmail.com>
2023-09-22 11:01:25 +0600

    [DONE] Semester Registration Validation

--- src/app/modules/semesterRegistration/semesterRegistration.service.ts
+++ src/app/modules/semesterRegistration/semesterRegistration.service.ts
@@ -1,9 +1,34 @@
-import { SemesterRegistration } from '@prisma/client';
+import {
+  SemesterRegistration,
+  SemesterRegistrationStatus,
+} from '@prisma/client';
+import httpStatus from 'http-status';
+import ApiError from '../../../errors/ApiError';
 import prisma from '../../../shared/prisma';
 
 const createSemesterRegistration = async (
   data: SemesterRegistration,
 ): Promise<SemesterRegistration> => {
+  const isAnySemesterRegistrationUpcommingOrOngoing =
+    await prisma.semesterRegistration.findFirst({
+      where: {
+        OR: [
+          {
+            status: SemesterRegistrationStatus.UPCOMING,
+          },
+          {
+            status: SemesterRegistrationStatus.ONGOING,
+          },
+        ],
+      },
+    });
+  if (isAnySemesterRegistrationUpcommingOrOngoing) {
+    throw new ApiError(
+      httpStatus.BAD_REQUEST,
+      `Thers is allrady an ${isAnySemesterRegistrationUpcommingOrOngoing.status} registration.`,
+    );
+  }
+
   const result = await prisma.semesterRegistration.create({
     data,
   });
