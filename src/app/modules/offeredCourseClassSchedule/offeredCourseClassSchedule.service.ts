eb37544f6d3518823bea284e8834f2622541c7d4
parvez13913 <parvezz13913@gmail.com>
2023-09-25 20:42:27 +0600

    [DONE] Offered Course class Schedule validation

--- src/app/modules/offeredCourseClassSchedule/offeredCourseClassSchedule.service.ts
+++ src/app/modules/offeredCourseClassSchedule/offeredCourseClassSchedule.service.ts
@@ -1,9 +1,11 @@
 import { OfferedCourseClassSchedule } from '@prisma/client';
 import prisma from '../../../shared/prisma';
+import { OfferedCourseClassScheduleUtils } from './offeredCourseClassSchedule.utils';
 
 const createOfferedCourseClassSchedule = async (
   data: OfferedCourseClassSchedule,
 ): Promise<OfferedCourseClassSchedule> => {
+  await OfferedCourseClassScheduleUtils.checkRoomAvailable(data);
   const result = await prisma.offeredCourseClassSchedule.create({
     data,
     include: {
