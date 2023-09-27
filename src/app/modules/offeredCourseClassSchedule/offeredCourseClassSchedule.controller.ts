9ce912874f0a8678a925ca6b71f0a17e928784bf
parvez13913 <parvezz13913@gmail.com>
2023-09-27 12:33:47 +0600

    [DONE] getAllOfferedCourseClassSchedule

--- src/app/modules/offeredCourseClassSchedule/offeredCourseClassSchedule.controller.ts
+++ src/app/modules/offeredCourseClassSchedule/offeredCourseClassSchedule.controller.ts
@@ -1,8 +1,11 @@
 import { OfferedCourseClassSchedule } from '@prisma/client';
 import { Request, Response } from 'express';
 import httpStatus from 'http-status';
+import { paginationFields } from '../../../constants/pagination';
 import catchAsync from '../../../shared/catchAsync';
+import pick from '../../../shared/pick';
 import sendResponse from '../../../shared/sendResponse';
+import { offeredCourseClassScheduleFilterableFieldas } from './offeredCourseClassSchedule.constants';
 import { OfferedCourseClassScheduleService } from './offeredCourseClassSchedule.service';
 
 const createOfferedCourseClassSchedule = catchAsync(
@@ -22,6 +25,30 @@ const createOfferedCourseClassSchedule = catchAsync(
   },
 );
 
+const getAllOfferedCourseClassSchedule = catchAsync(
+  async (req: Request, res: Response) => {
+    const filters = pick(
+      req.query,
+      offeredCourseClassScheduleFilterableFieldas,
+    );
+    const options = pick(req.query, paginationFields);
+    const result =
+      await OfferedCourseClassScheduleService.getAllOfferedCourseClassSchedule(
+        filters,
+        options,
+      );
+
+    sendResponse<OfferedCourseClassSchedule[]>(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: 'Offered course Schedule Fetched Successfully!',
+      meta: result.meta,
+      data: result.data,
+    });
+  },
+);
+
 export const OfferedCourseClassScheduleController = {
   createOfferedCourseClassSchedule,
+  getAllOfferedCourseClassSchedule,
 };
--- src/app/modules/offeredCourseClassSchedule/offeredCourseClassSchedule.service.ts
+++ src/app/modules/offeredCourseClassSchedule/offeredCourseClassSchedule.service.ts
@@ -1,5 +1,15 @@
-import { OfferedCourseClassSchedule } from '@prisma/client';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { OfferedCourseClassSchedule, Prisma } from '@prisma/client';
+import { paginationHelpers } from '../../../helpers/paginationHelper';
+import { IGenericResponse } from '../../../interfaces/common';
+import { IPaginationOptions } from '../../../interfaces/pagination';
 import prisma from '../../../shared/prisma';
+import {
+  offeredCourseClassScheduleFilterableFieldas,
+  offeredCourseClassScheduleRelationalFields,
+  offeredCourseClassScheduleRelationalFieldsMapper,
+} from './offeredCourseClassSchedule.constants';
+import { IOfferedCourseClassScheduleFilters } from './offeredCourseClassSchedule.interface';
 import { OfferedCourseClassScheduleUtils } from './offeredCourseClassSchedule.utils';
 
 const createOfferedCourseClassSchedule = async (
@@ -19,6 +29,80 @@ const createOfferedCourseClassSchedule = async (
   return result;
 };
 
+const getAllOfferedCourseClassSchedule = async (
+  filters: IOfferedCourseClassScheduleFilters,
+  options: IPaginationOptions,
+): Promise<IGenericResponse<OfferedCourseClassSchedule[]>> => {
+  const { searchTerm, ...filterData } = filters;
+  const { page, limit, skip } = paginationHelpers.calculatePagination(options);
+
+  const andCoditions = [];
+
+  if (searchTerm) {
+    andCoditions.push({
+      OR: offeredCourseClassScheduleFilterableFieldas.map(field => ({
+        [field]: {
+          contains: searchTerm,
+          mode: 'insensitive',
+        },
+      })),
+    });
+  }
+
+  if (Object.keys(filterData).length > 0) {
+    andCoditions.push({
+      AND: Object.keys(filterData).map(key => {
+        if (offeredCourseClassScheduleRelationalFields.includes(key)) {
+          return {
+            [offeredCourseClassScheduleRelationalFieldsMapper[key]]: {
+              id: (filterData as any)[key],
+            },
+          };
+        } else {
+          return {
+            [key]: {
+              equals: (filterData as any)[key],
+            },
+          };
+        }
+      }),
+    });
+  }
+
+  const whereConditions: Prisma.OfferedCourseClassScheduleWhereInput =
+    andCoditions.length > 0 ? { AND: andCoditions } : {};
+
+  const result = await prisma.offeredCourseClassSchedule.findMany({
+    where: whereConditions,
+    skip,
+    take: limit,
+    orderBy:
+      options.sortBy && options.sortOrder
+        ? { [options.sortBy]: options.sortOrder }
+        : { createdAt: 'desc' },
+    include: {
+      faculty: true,
+      room: true,
+      semesterRegistration: true,
+      offeredCourseSection: true,
+    },
+  });
+
+  const total = await prisma.offeredCourseClassSchedule.count({
+    where: whereConditions,
+  });
+
+  return {
+    meta: {
+      page,
+      limit,
+      total,
+    },
+    data: result,
+  };
+};
+
 export const OfferedCourseClassScheduleService = {
   createOfferedCourseClassSchedule,
+  getAllOfferedCourseClassSchedule,
 };
