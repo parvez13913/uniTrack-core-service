66516f7a570f7eddc0ad17e921b40c1f728f18c6
parvez13913 <parvezz13913@gmail.com>
2023-11-28 11:22:00 +0600

    [DONE] student enrolled course validation

--- src/app/modules/studentEnrolledCourseMark/studentEnrolledCourseMark.route.ts
+++ src/app/modules/studentEnrolledCourseMark/studentEnrolledCourseMark.route.ts
@@ -1,13 +1,16 @@
 import express from 'express';
 import { ENUM_USER_ROLE } from '../../../enums/user';
 import auth from '../../middlewares/auth';
+import validateRequest from '../../middlewares/validateRequest';
 import { StudentEnrolledCourseMarkController } from './studentEnrolledCourseMark.controller';
+import { StudentEnrolledCourseMarkValidation } from './studentEnrolledCourseMark.validation';
 
 const router = express.Router();
 
 router.patch(
   '/update-marks',
   auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.FACULTY),
+  validateRequest(StudentEnrolledCourseMarkValidation.updateStudentMarks),
   StudentEnrolledCourseMarkController.updateStudentMarks,
 );
 
@@ -20,6 +23,9 @@ router.patch(
 router.patch(
   '/update-final-marks',
   auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.FACULTY),
+  validateRequest(
+    StudentEnrolledCourseMarkValidation.updateStudentCourseFinalMarks,
+  ),
   StudentEnrolledCourseMarkController.updateFinalMarks,
 );
 
