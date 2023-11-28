e0e55ef4385e79e9ff0641c93026b79c56f15351
parvez13913 <parvezz13913@gmail.com>
2023-11-28 12:26:04 +0600

    OfferedCourse Route updated

--- src/app/modules/offeredCourse/offeredCourse.route.ts
+++ src/app/modules/offeredCourse/offeredCourse.route.ts
@@ -1,26 +1,33 @@
 import express from 'express';
+import { ENUM_USER_ROLE } from '../../../enums/user';
+import auth from '../../middlewares/auth';
 import validateRequest from '../../middlewares/validateRequest';
 import { OfferedCourseController } from './offeredCourse.controller';
 import { OfferedCourseValidation } from './offeredCourse.validation';
 
 const router = express.Router();
 
+router.get('/', OfferedCourseController.getAllOfferedCourses);
+router.get('/:id', OfferedCourseController.getSingleOfferedCourse);
+
 router.post(
   '/create-offered-course',
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
   validateRequest(OfferedCourseValidation.createOfferedCourseZodSchema),
   OfferedCourseController.createOfferedCourses,
 );
 
-router.get('/:id', OfferedCourseController.getSingleOfferedCourse);
-
 router.patch(
   '/:id',
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
   validateRequest(OfferedCourseValidation.updateOfferedCourseZodSchema),
   OfferedCourseController.updateOfferedCourse,
 );
 
-router.delete('/:id', OfferedCourseController.deleteOfferedCourse);
-
-router.get('/', OfferedCourseController.getAllOfferedCourses);
+router.delete(
+  '/:id',
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  OfferedCourseController.deleteOfferedCourse,
+);
 
 export const OfferedCourseRouter = router;
