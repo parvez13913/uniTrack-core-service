c3ea85768b5d30c826c1f7c724cb589a376f069c
parvez13913 <parvezz13913@gmail.com>
2023-11-28 12:54:44 +0600

    update offered course class schedule route

--- src/app/modules/offeredCourseClassSchedule/offeredCourseClassSchedule.route.ts
+++ src/app/modules/offeredCourseClassSchedule/offeredCourseClassSchedule.route.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import { ENUM_USER_ROLE } from '../../../enums/user';
+import auth from '../../middlewares/auth';
 import validateRequest from '../../middlewares/validateRequest';
 import { OfferedCourseClassScheduleController } from './offeredCourseClassSchedule.controller';
 import { OfferedCourseClassScheduleValidation } from './offeredCourseClassSchedule.validation';
@@ -19,6 +21,7 @@ router.post(
   validateRequest(
     OfferedCourseClassScheduleValidation.createOfferedCourseClassSchedule,
   ),
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
   OfferedCourseClassScheduleController.createOfferedCourseClassSchedule,
 );
 
@@ -27,11 +30,13 @@ router.patch(
   validateRequest(
     OfferedCourseClassScheduleValidation.updateOfferedCourseClassSchedule,
   ),
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
   OfferedCourseClassScheduleController.updateOfferedCourseClassSchedule,
 );
 
 router.delete(
   '/:id',
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
   OfferedCourseClassScheduleController.deleteOfferedCourseClassSchedule,
 );
 
